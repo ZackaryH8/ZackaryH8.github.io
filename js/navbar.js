bf0ba9653662568797435f7007b5aec4fb8c4be8
ZackaryH8 <zackary12389@yahoo.co.uk>
2020-08-02 18:42:12 +0100

    Created Navbar

---
+++ js/navbar.js
@@ -0,0 +1,6 @@
+const navBar = document.getElementById("js-menu");
+const navBarToggle = document.getElementById("js-navbar-toggle");
+
+function toggleNavBar() {
+  navBar.classList.toggle("active");
+}
