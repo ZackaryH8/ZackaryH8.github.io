9341e22ca97c537022ae210b4c8dfee4cc4ea1dc
ZackaryH8 <zackary12389@yahoo.co.uk>
2020-08-02 19:00:57 +0100

    Save dark mode toggle to localstorage

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,3 +1,21 @@
+let darkMode = localStorage.getItem("darkToggle");
+
+window.addEventListener("DOMContentLoaded", () => {
+    if (darkMode == null) {
+        localStorage.setItem("darkToggle", 0);
+        document.body.classList.remove("dark");
+    } else if (darkMode == 0) {
+        document.body.classList.remove("dark");
+    } else if (darkMode == 1) {
+        document.body.classList.add("dark");
+    }
+});
+
 function toggleDark() {
     document.body.classList.toggle("dark");
+    if (darkMode == 0) {
+        localStorage.setItem("darkToggle", 1);
+    } else {
+        localStorage.setItem("darkToggle", 0);
+    }
 }
